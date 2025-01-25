5632c3435530dbb53ccb04149310e3643f836d06
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-01-25 14:45:31 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
 $(function() {
 
-    console.log(1)
+    // Fancybox.bind("[data-fancybox]", {
+    //     // Your custom options
+    //     // animated: false
+    // });
 
-    Fancybox.bind("[data-fancybox]", {
-        // Your custom options
-        // animated: false
-    });
+    // $(".btn").fancybox();
+
+    $('.news__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        speed: 500,
+
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+
+        responsive: [
+            {
+                breakpoint: 1416,
+                settings: {
+                  
+                //   centerMode: true,
+                //   centerPadding: '40px',
+                  slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    // arrows: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
+      });
 
 });
