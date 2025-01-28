4fc48f3d69e91a4f31a4506b9eac967ccf0b1f95
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-01-28 13:03:13 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(function() {
 
+    $('.faq__ask').on('click', function () {
+        const answer = $(this).next();
+    
+        if (!$(this).hasClass('active')){
+            $('.faq__ask').removeClass('active');
+            $('.faq__answer').slideUp(300);
+            $(this).addClass('active');
+            answer.slideDown(300);
+        } else {
+            $(this).removeClass('active');
+            answer.slideUp(300);
+        }
+    });
+
     // $(".fancyone").fancybox();
 
     $('[data-fancybox]').fancybox({
