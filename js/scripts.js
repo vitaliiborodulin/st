88c9fdeca2d528f56b22601830657c5385210e25
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-02-13 18:47:44 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -146,4 +146,16 @@ $(function() {
 
     // $(".fancyone").fancybox();
 
+    $('select').niceSelect();
+
+    document.addEventListener( 'wpcf7mailsent', function( event ) {
+        // if(event.detail.contactFormId=="224"){
+        // onclick=yaCounter26043108.reachGoal('formsend'); return true;
+        // }
+    
+        $(".form form").hide();
+        $(".form .form__thank").show();
+    
+    }, false );
+
 });
