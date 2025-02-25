e1ffec612c445327e8479915b6c0c4adf68a7e99
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-02-25 20:44:26 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -204,15 +204,46 @@ $(function() {
     
     	tabCards.removeClass('active');
     	$(this).addClass('active');
-    
+    	
     	var id = $(this).data('open');
-    
+    	
     	var complTabs = $('.tab__rights .tab__right')
-    
+    	
     	complTabs.slideUp(200);
     	$('.tab__rights .tab__right[data-id='+id+']').slideDown(200);
+    	checkTab();
     
     });
+    
+    function checkInputs(){
+    	let sum = 0;
+    	let inputs =  querySelectorAll('.single-dop__item input[type=checkbox]');
+    	// $.each(inputs, function(key, val){
+    	// 	console.log(key, val)
+    	// })
+    	console.log(inputs)
+    	// for (index = 0; index < inputs.length; ++index) {
+    	// 	console.log(inputs[index]);
+    	// }
+    	// for (let i = 0; i++)
+    	// if (inputs.is(':checked')){
+    	// 	sum += inputs.data('price');
+    	// }
+    }
+    checkInputs();
+    
+    function checkTab() {
+    	let mainPrice = $('.tab__card.active').data('price');
+    	$('.price').attr('data-price', mainPrice);
+    	$('.price').text(prettify(mainPrice));
+    
+    	let dopPricce =  $('.dop-price').data('price');
+    	
+    	$('.final-price').text(prettify(mainPrice + dopPricce) + ' ₽');
+    	$('.final-price').attr('data-price', mainPrice + dopPricce);
+    }
+    
+    checkTab();
     let price = $('.price'); //цена комплектации дома
     let dopPrice = $('.dop-price'); //цена допов
     let finalPrice = $('.final-price'); //финальная цена
@@ -248,6 +279,10 @@ $(function() {
         var n = num.toString();
         return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
     }
+    
+    // setPrice($('.tab__card.active'));
+    // setTimeout(setPrice($('.tab__card.active'), 5000));
+    // setTimeout(setSum, 2000);
     /* Значения из текстовых инпутов */
     const totalCost = document.getElementById('cost'),
         anInitialFee = document.getElementById('initial'),
@@ -321,5 +356,6 @@ $(function() {
         $(".form .form__thank").show();
     
     }, false );
+    
 
 });
