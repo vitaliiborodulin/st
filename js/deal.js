8e191620d047121b3233e36fae4a9e4321b39dcf
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-08-18 12:05:04 +0300

    Updates

---
+++ js/deal.js
@@ -0,0 +1,212 @@
+$(function() {
+
+// надписи и цвета на секторах
+const prizes = [
+    // {
+    //   text: "10% off",
+    //   color: "hsl(197 30% 43%)",
+    // },
+    // { 
+    //   text: "Дизайн в подарок",
+    //   color: "hsl(173 58% 39%)",
+    // },
+    // { 
+    //   text: "Второй сайт бесплатно",
+    //   color: "hsl(43 74% 66%)",
+    // },
+    // {
+    //   text: "Скидка 50%",
+    //   color: "hsl(27 87% 67%)",
+    // },
+    {
+      text: "✨ Антисеп-тирование",
+      // color: "hsl(12 76% 61%)",
+      color: "#FC622C",
+    },
+    {
+      text: "🎨 Покраска фасада",
+      // color: "hsl(350 60% 52%)",
+      color: "#2aabee",
+    },
+    {
+      text: "🏠 Фундамент",
+      // color: "hsl(91 43% 54%)",
+      color: "#00cf64",
+    },
+    {
+      text: "🚿 Водосточная система",
+      // color: "hsl(140 36% 74%)",
+      color: "#696868ff",
+    },
+    {
+      text: "❄️ Снего-задержание",
+      // color: "hsl(201 85% 55%)",
+      color: "#70ABCB",
+    }
+  ];
+  
+  // создаём переменные для быстрого доступа ко всем объектам на странице — блоку в целом, колесу, кнопке и язычку
+  const wheel = document.querySelector(".deal-wheel");
+  const spinner = wheel.querySelector(".spinner");
+  const trigger = wheel.querySelector(".btn-spin");
+  const ticker = wheel.querySelector(".ticker");
+  
+  // на сколько секторов нарезаем круг
+  const prizeSlice = 360 / prizes.length;
+  // на какое расстояние смещаем сектора друг относительно друга
+  const prizeOffset = Math.floor(180 / prizes.length);
+  // прописываем CSS-классы, которые будем добавлять и убирать из стилей
+  const spinClass = "is-spinning";
+  const selectedClass = "selected";
+  // получаем все значения параметров стилей у секторов
+  const spinnerStyles = window.getComputedStyle(spinner);
+  
+  // переменная для анимации
+  let tickerAnim;
+  // угол вращения
+  let rotation = 0;
+  // текущий сектор
+  let currentSlice = 0;
+  // переменная для текстовых подписей
+  let prizeNodes;
+  
+  // расставляем текст по секторам
+  const createPrizeNodes = () => {
+    // обрабатываем каждую подпись
+    prizes.forEach(({ text, color, reaction }, i) => {
+      // каждой из них назначаем свой угол поворота
+      const rotation = ((prizeSlice * i) * -1) - prizeOffset;
+      // добавляем код с размещением текста на страницу в конец блока spinner
+      spinner.insertAdjacentHTML(
+        "beforeend",
+        // текст при этом уже оформлен нужными стилями
+        `<li class="prize" data-reaction=${reaction} style="--rotate: ${rotation}deg">
+          <span class="text">${text}</span>
+        </li>`
+      );
+    });
+  };
+  
+  // рисуем разноцветные секторы
+  const createConicGradient = () => {
+    // устанавливаем нужное значение стиля у элемента spinner
+    spinner.setAttribute(
+      "style",
+      `background: conic-gradient(
+        from -90deg,
+        ${prizes
+          // получаем цвет текущего сектора
+          .map(({ color }, i) => `${color} 0 ${(100 / prizes.length) * (prizes.length - i)}%`)
+          .reverse()
+        }
+      );`
+    );
+  };
+  
+  // создаём функцию, которая нарисует колесо в сборе
+  const setupWheel = () => {
+    // сначала секторы
+    createConicGradient();
+    // потом текст
+    createPrizeNodes();
+    // а потом мы получим список всех призов на странице, чтобы работать с ними как с объектами
+    prizeNodes = wheel.querySelectorAll(".prize");
+  };
+  
+  // определяем количество оборотов, которое сделает наше колесо
+  const spinertia = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
+  // функция запуска вращения с плавной остановкой
+  const runTickerAnimation = () => {
+    // взяли код анимации отсюда: https://css-tricks.com/get-value-of-css-rotation-through-javascript/
+    const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
+    const a = values[0];
+    const b = values[1];  
+    let rad = Math.atan2(b, a);
+    
+    if (rad < 0) rad += (2 * Math.PI);
+    
+    const angle = Math.round(rad * (180 / Math.PI));
+    const slice = Math.floor(angle / prizeSlice);
+  
+    // анимация язычка, когда его задевает колесо при вращении
+    // если появился новый сектор
+    if (currentSlice !== slice) {
+      // убираем анимацию язычка
+      ticker.style.animation = "none";
+      // и через 10 миллисекунд отменяем это, чтобы он вернулся в первоначальное положение
+      setTimeout(() => ticker.style.animation = null, 10);
+      // после того, как язычок прошёл сектор - делаем его текущим 
+      currentSlice = slice;
+    }
+    // запускаем анимацию
+    tickerAnim = requestAnimationFrame(runTickerAnimation);
+  };
+  
+  // функция выбора призового сектора
+  const selectPrize = () => {
+    const selected = Math.floor(rotation / prizeSlice);
+    prizeNodes[selected].classList.add(selectedClass);
+  };
+
+  
+  // отслеживаем нажатие на кнопку
+  trigger.addEventListener("click", () => {
+    // делаем её недоступной для нажатия
+    trigger.disabled = true;
+    // задаём начальное вращение колеса
+    rotation = Math.floor(Math.random() * 360 + spinertia(2000, 5000));
+    // убираем прошлый приз
+    prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
+    // добавляем колесу класс "is-spinning", с помощью которого реализуем нужную отрисовку
+    wheel.classList.add(spinClass);
+    // через CSS говорим секторам, как им повернуться
+    spinner.style.setProperty("--rotate", rotation);
+    // возвращаем язычок в горизонтальную позицию
+    ticker.style.animation = "none";
+    // запускаем анимацию вращение
+    runTickerAnimation();
+  });
+  
+  // отслеживаем, когда закончилась анимация вращения колеса
+  spinner.addEventListener("transitionend", () => {
+    // останавливаем отрисовку вращения
+    cancelAnimationFrame(tickerAnim);
+    // получаем текущее значение поворота колеса
+    rotation %= 360;
+    // выбираем приз
+    selectPrize();
+    // убираем класс, который отвечает за вращение
+    wheel.classList.remove(spinClass);
+    // отправляем в CSS новое положение поворота колеса
+    spinner.style.setProperty("--rotate", rotation);
+    // делаем кнопку снова активной
+    trigger.disabled = false;
+
+    $(".btn-spin").hide(300);
+
+
+    const prize = document.querySelector('.prize.selected .text').textContent;
+
+
+    const waLink = document.createElement('a');
+    waLink.setAttribute("href", "https://wa.me/+79110371411?text=Привет, я участвовал в розыгрыше и выйграл " + prize + ', свяжитесь со мной');
+    waLink.setAttribute('target', '_blank');
+    waLink.textContent = 'Написать в WhatsApp';
+    waLink.classList.add('btn');
+    waLink.classList.add('wa-btn');
+
+    document.querySelector(".more").append(waLink);
+
+    $(".more").show(300);
+
+  });
+  
+  // подготавливаем всё к первому запуску
+  setupWheel();
+
+});
--- js/scripts.js
+++ js/scripts.js
@@ -224,43 +224,8 @@ $(function() {
       //       }
       //   ]
       // });
-    if(document.querySelector('.tab__cards')){
+    if(document.querySelector('.compl__list')){
     
-    const tabCards = $('.tab__cards .tab__card');
-    
-    tabCards.on('click', function(){
-    
-    	tabCards.removeClass('active');
-    	$(this).addClass('active');
-    	
-    	const open = $(this).attr('data-open');
-    	$('#get-projects input[name=complect]').val(open);
-    	
-    	var id = $(this).data('open');
-    	
-    	var complTabs = $('.tab__rights .tab__right')
-    	
-    	complTabs.slideUp(200);
-    	$('.tab__rights .tab__right[data-id='+id+']').slideDown(200);
-    	checkTab();
-    
-    });
-    
-    
-    // проверяет активный tab и ставит по нему цену комплектации и финальную цену
-    function checkTab() {
-    	let mainPrice = $('.tab__card.active').data('price');
-    	$('.price').attr('data-price', mainPrice);
-    	$('.price').text(prettify(mainPrice));
-    
-    	let dopPricce =  $('.dop-price').data('price');
-    	checkSum();
-    	
-    }
-    
-    	checkTab();
-    
-    }
     const compls = $('.compl__item');
     
     compls.on('click', function(){
@@ -276,9 +241,29 @@ $(function() {
         } else {
             checkboxDop.prop('checked', true);
             $(this).addClass('active');
-           
+    
         }
+    
+    	checkTab();
     });
+    
+    // проверяет активный tab и ставит по нему цену комплектации и финальную цену
+    function checkTab() {
+    	let mainPrice = $('.compl__item.active').data('price');
+    	$('.price').attr('data-price', mainPrice);
+    	$('.price').text(prettify(mainPrice));
+    
+    	let open = $('.compl__item.active').data('open');
+    	$('#get-projects input[name=complect]').val(open);
+    
+    	let dopPricce =  $('.dop-price').data('price');
+    	checkSum();
+    	
+    }
+    
+    	checkTab();
+    
+    }
     let price = $('.price'); //цена комплектации дома
     let dopPrice = $('.dop-price'); //цена допов
     let finalPrice = $('.final-price'); //финальная цена
