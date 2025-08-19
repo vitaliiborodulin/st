7929034ae7ad2542dd1831c77a4fc0ed8d80b65a
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-08-19 16:16:03 +0300

    Updates

--- js/deal.js
+++ js/deal.js
@@ -216,11 +216,30 @@ const prizes = [
 
 });
 
-setTimeout(function(){ 
-    // const fancybox = new Fancybox([
-    //     {src: "#deal"}
-    // ]);
-    $.fancybox.open({
-      src  : '#deal'
+// setTimeout(function(){ 
+//     // const fancybox = new Fancybox([
+//     //     {src: "#deal"}
+//     // ]);
+//     $.fancybox.open({
+//       src  : '#deal'
+//     });
+// }, 5000);
+
+    // var visit = get_cookie("lawyer_visited");
+    $(document).mouseleave(function (event) {
+    	// if (get_cookie("lawyer_visited") != '1'){
+    		event = event || window.event;
+    		if (event.clientY < 0 || event.clientY < 3) {
+    				
+    			$.fancybox.open({
+            src  : '#deal'
+          });
+          
+    			// var date1 = new Date();
+    			// date1.setDate(date1.getDate() + 7);
+    			// date1 = date1.toUTCString();
+    			// document.cookie = "lawyer_visited=1;expires=" + date1;
+    
+    			}
+    	// }
     });
-}, 5000);
